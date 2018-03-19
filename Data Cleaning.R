e3870093ef4bb630dd55ffe8cd62ebc7199341f8
Christopher Bolte <christopher.bolte@gmail.com>
2018-03-19 15:19:24 -0800

    Not quite working. Adding duplicate detections, but otherwise seems to be working well.

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -1,12 +1,14 @@
 #Make Basic Filters for eliminating bad detections
 
-#1. If a single movement is more than 100 rkm, then remove the detections of the receiver that has currently arrived.
+#1. If a single movement is more than a specified rkm, then remove the detections of the receiver that has currently arrived.
 
-clip_and_replace_middle <- function(df_all_detections, df_tag_list, taglist_colname, upmove_rkm = 50, low_rkm = 0, high_rkm = 540) {
+clean_bad_receivers <- function(df_all_detections, df_tag_list, taglist_colname, upmove_rkm = 50, low_rkm = 0, high_rkm = 540) {
   all_clean_detections <- NULL
-  det <- subset(df_all_detections, Detect_rkm > low_rkm & Detect_rkm < high_rkm) #use det from here on out
+  det <- df_all_detections
   det <- det[is.na(det$DetectDate) == FALSE, ]
   det <- det[is.na(det$Detect_rkm) == FALSE, ]
+  det$Detect_rkm <- as.numeric(det$Detect_rkm)
+  det <- subset(det, Detect_rkm > low_rkm & Detect_rkm < high_rkm) #use det from here on out
   det$Detect_rkm <- as.numeric(det$Detect_rkm)
   all_tags <- unique(df_tag_list[[taglist_colname]])
   for (tag in all_tags) {
@@ -15,43 +17,70 @@ clip_and_replace_middle <- function(df_all_detections, df_tag_list, taglist_coln
     ind_tag <- subset(det, TagID == tag)
     ind_tag <- ind_tag[order(ind_tag$DetectDate), ]
     all_rows <- c(1:nrow(ind_tag))
+    restriction <- 0
+    #print(length(all_rows))
     if (nrow(ind_tag) > 0) {
       for (row in all_rows) { #This will identify where the detections in that receiver start
-        if (row == nrow(ind_tag)) {
+        if (row == nrow(ind_tag)) { #end the current iteration of the for-loop because nothing fits the other criteria.
           print("There are no errant detections in the middle of this detection history above the specified threshold.")
           all_clean_detections <- rbind(all_clean_detections, ind_tag)
+          break
+        }
+        if (restriction == 1) {
+          print('Multiple Large Movements in this Tag, Run Script Again')
           break
         }
         first_rkm <- ind_tag$Detect_rkm[row]
         second_rkm <- ind_tag$Detect_rkm[row + 1]
         diff_rkm <- abs(second_rkm - first_rkm)
-        if (first_rkm != second_rkm & abs(diff_rkm) > upmove_rkm) {
+        if (first_rkm != second_rkm & diff_rkm > upmove_rkm) {
+          restriction <- 1
+          print("Triggering Condition")
           #If there is a difference greater than the specified threshold for a single movement, the following needs to occur
           #1. Identify the detection before the change.
           #2. Determine how many (which rows) detections are on the errant receiver before it changes.
           #3. Remove these detections
           #4. Continue at location where original row. is it possible to reset where the row is?
-          start_bad_det <- row + 1
-          new_rows <- c(start_bad_det:nrow(ind_tag))
+          start_bad_det <- row + 1 #this is the beginning of the bad detections from an errant receiver.
+          #print(ind_tag[start_bad_det, ])
+          new_rows <- c(start_bad_det:nrow(ind_tag)) #Gets detections from the start of the bad receiver to the end of that TagIDs detections
+          #print(head(ind_tag))
           for (rows in new_rows) {
-            bad_rkm <- ind_tag[rows]
-            next_rkm <- ind_tag[rows + 1]
-            if (bad_rkm != next_rkm) { #we have reached the end of the bad receiver/detection
-              #remove detections from start_bad_det to bad_rkm
-              ind_tag <- ind_tag[-(start_bad_det:bad_rkm), ] #ind_tag is now cleaned.
-              all_clean_detections <- rbind(all_clean_detections, ind_tag)
+            #print(rows)
+            bad_rkm <- ind_tag$Detect_rkm[rows]
+            next_rkm <- ind_tag$Detect_rkm[rows + 1]
+            if (rows == nrow(ind_tag)) {
+              #print('End Second Iteration')
               break
             }
-          }
-        }
-      }
-    }
-  }
+            if (bad_rkm != next_rkm) { #we have reached the end of the bad receiver/detection
+              #remove detections from start_bad_det to bad_rkm
+              #print('End Bad Receiver')
+              #print(start_bad_det)
+              #print(bad_rkm)
+              new_ind_tag <- ind_tag[-c(start_bad_det:rows), ] #ind_tag is now cleaned.
+              all_clean_detections <- rbind(all_clean_detections, new_ind_tag)
+              break
+            } #end of second row iteration.
+          }#Iterate through rows after meeting below condition
+        } #If statement if diff_rkm and > threshold
+      } #Iterate by row in an individual tag
+    } #If nrow > 0 statement
+  } #Initial For Loop (iterate by TagID)
   return(all_clean_detections)
 }
 
+ind_striped <- subset(striped, TagID == 820)
 
-new_smb <- clip_and_replace_middle(smb, smb, 'TagID', 100)
+new_striped <- clean_bad_receivers(df_all_detections = ind_striped, 
+                                   df_tag_list = ind_striped,
+                                   taglist_colname = 'TagID', upmove_rkm = 100)
 
 #For next time, identify fish that will set off each criteria, then make sure that they can be added to eachother using the script.
+#Get Sample Fish that fit each of the criteria.
+
+
+
+ind_striped$Detect_rkm <- as.numeric(ind_striped$Detect_rkm)
+test_det <- subset(ind_striped, Detect_rkm > 0 & Detect_rkm < 540)
 
